387242da02e7acccdf28e301a3d936e951ed8b5d
Timothy Moore <mtimothy984@gmail.com>
2020-02-29 16:51:49 -0800

    Add initial runner structure
    This does not include any actual summons yet

---
+++ src/main.py
@@ -0,0 +1,59 @@
+"""Main entry point into the application. This starts up each of the
+subprocesses, which are capable of queueing jobs, then acts as a jobs worker.
+This approach allows the loansbot to be parallelized on queries but
+single-threaded for mutations.
+"""
+import helper
+import os
+from lblogging import Logger, Level
+from multiprocessing import Process
+import importlib
+
+
+SUBPROCESSES = ('runners.comments',)
+
+
+def main():
+    """Spawn all of the subprocesses as daemons and then works jobs until one
+    one them dies or a signal to shutdown is received."""
+    connection = helper.connect_to_database()
+    logger = Logger(os.environ['APPNAME'], 'main.py', connection)
+    logger.prepare()
+
+    helper.setup_clean_shutdown(logger, [])
+
+    # We connect to the amqp service here to verify it's up
+    amqp = helper.connect_to_amqp(logger)
+    amqp.close()
+    amqp = None
+
+    logger.print(Level.TRACE, 'Spawning subprocesses...')
+    logger.connection.commit()
+    subprocs = []
+    for modnm in SUBPROCESSES:
+        logger.print(Level.TRACE, 'Spawning subprocess {}', modnm)
+        mod = importlib.import_module(modnm)
+        proc = Process(target=mod.main, daemon=True)
+        proc.start()
+        subprocs.append(proc)
+    logger.connection.commit()
+
+    print('Successfully started up')
+    logger.print(Level.INFO, 'Successfully started up!')
+    logger.connection.commit()
+
+    while True:
+        running = True
+        for proc in subprocs:
+            if not proc.is_alive():
+                logger.print(Level.ERROR, 'A child process has died! Terminating...')
+                logger.connection.commit()
+                running = False
+                break
+        if not running:
+            break
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/summons/summon.py
@@ -0,0 +1,35 @@
+"""Describes the generic interface that summons are expected to support"""
+
+class Summon:
+    """An operation which can be triggered by comments or link posts on reddit.
+
+    :param name: A unique name for this summon.
+    """
+    def might_apply_to_comment(self, comment):
+        """Determines if this summon applies to the given comment. This should
+        be fairly fast, since every comment will be checked by every summon.
+        Further, this should have no side-effects.
+
+        :param comment: The comment dictionary from the reddit-proxy
+        :type comment: dict
+        :returns: True if this summon might apply to the given comment and
+            false otherwise.
+        """
+        raise NotImplementedError()
+
+    def handle_comment(self, logger, conn, amqp, channel, comment):
+        """Handles the given comment. This is expected to have side-effects,
+        such as saving things to the database, logging, and responding via
+        reddit using the reddit proxy.
+
+        :param logger: The logger to use. This function should not assume
+            that the logger is on the same database as everything else. It
+            should also not assume it isn't on the same database as everything
+            else. The logger will not auto-commit.
+        :param conn: The database connection to use for non-logging related
+            calls.
+        :param amqp: The advanced message queue protocol connection
+        :param channel: The channel within the advanced message queue to use
+        :param comment: The comment that needs to be handled
+        """
+        raise NotImplementedError()
