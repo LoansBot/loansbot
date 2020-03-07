554656dff1917d54948fc78799beaaaa6344eb47
Timothy Moore <mtimothy984@gmail.com>
2020-03-07 09:24:05 -0800

    Skip early break with no summon
    This causes the fullname not to get inserted into handled_fullnames

--- src/runners/comments.py
+++ src/runners/comments.py
@@ -89,13 +89,9 @@ def scan_for_comments(conn, cursor, logger, amqp, version, summons):
                 break
 
             num_to_find = num_to_find - 1
-            if summon_to_use is None:
-                if num_to_find <= 0:
-                    break
-                continue
-            # TODO check author
-
             if summon_to_use is not None:
+                # TODO check author
+
                 logger.print(Level.DEBUG, 'Using summon {}', summon_to_use.name)
                 try:
                     summon_to_use.handle_comment(logger, conn, amqp, channel, comment)
