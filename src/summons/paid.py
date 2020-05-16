75f5cd256320ae6bd757fb86f7470154cef8d966
Timothy Moore <mtimothy984@gmail.com>
2020-05-16 07:25:53 -0700

    Add simple paid command implementation
    This implements the simplified paid command, which is easier
    for those that don't have multiple outstanding
    loans with a single person

--- src/summons/paid.py
+++ src/summons/paid.py
@@ -6,6 +6,11 @@ from .summon import Summon
 from parsing.parser import Parser
 import parsing.ext_tokens
 import utils.reddit_proxy
+import loan_format_helper
+import utils.paid_utils
+from pypika import Table, Parameter, Order
+from lblogging import Level
+from lbshared.responses import get_response
 
 
 PARSER = Parser(
@@ -35,14 +40,84 @@ class PaidSummon(Summon):
         borrower_username = token_vals[0]
         amt = token_vals[1]
 
+        comment_permalink = 'https://reddit.com/comments/{}/redditloans/{}'.format(
+            comment['link_fullname'][3:],
+            comment['fullname'][3:]
+        )
+
+        loans = Table('loans')
+        lenders = Table('lenders')
+        borrowers = Table('borrowers')
+
+        effected_loans_pre = []
+        effected_loans_post = []
+        remaining = amt
+        while remaining.minor > 0:
+            itgs.write_cursor.execute(
+                loan_format_helper.create_loans_query()
+                .where(lenders.username == Parameter('%s'))
+                .where(borrowers.username == Parameter('%s'))
+                .where(loans.repaid_at.isnull())
+                .orderby(loans.created_at, order=Order.asc)
+                .limit(1)
+                .get_sql(),
+                (lender_username.lower(), borrower_username.lower())
+            )
+            row = itgs.write_cursor.fetchone()
+            if row is None:
+                break
+            loan_pre = loan_format_helper.fetch_loan(row)
+            old_minor = remaining.minor
+            (_, _, remaining) = utils.paid_utils.apply_repayment(itgs, loan_pre.id, remaining)
+            itgs.write_cursor.execute(
+                loan_format_helper.create_loans_query()
+                .where(loans.id == Parameter('%s'))
+                .get_sql(),
+                (loan_pre.id,)
+            )
+            row = itgs.write_cursor.fetchone()
+            if row is None:
+                itgs.logger.print(
+                    Level.WARN,
+                    'Somehow, while handling the paid summon by /u/{} at {}, ' +
+                    'the loan was deleted while applying repayment. We stopped ' +
+                    'propagating the loan early. If nobody was deleting loans this ' +
+                    'is definitely developer error.',
+                    lender_username, comment_permalink
+                )
+                effected_loans_pre.append(loan_pre)
+                break
+            loan_post = loan_format_helper.fetch_loan(row)
+            if old_minor <= remaining.minor:
+                # Sanity check to prevent loops
+                break
+            effected_loans_pre.append(loan_pre)
+            effected_loans_post.append(loan_post)
+
+        itgs.logger.print(
+            Level.INFO,
+            '/u/{} was repaid by /u/{} by {} over {} loan%s at {}',
+            lender_username, borrower_username, amt,
+            len(effected_loans_pre), 's' if len(effected_loans_pre) != 1 else '',
+            comment_permalink
+        )
+
+        formatted_response = get_response(
+            itgs,
+            'paid',
+            lender_username=lender_username,
+            borrower_username=borrower_username,
+            loans_before=loan_format_helper.format_loan_table(effected_loans_pre),
+            loans_after=loan_format_helper.format_loan_table(effected_loans_post),
+            num_loans_affected=len(effected_loans_pre),
+            amount=str(amt),
+            remaining=str(remaining)
+        )
+
         utils.reddit_proxy.send_request(
             itgs, rpiden, rpversion, 'post_comment',
             {
                 'parent': comment['fullname'],
-                'text': (
-                    'Detected that /u/{} was repaid {} by /u/{}'.format(
-                        lender_username, amt, borrower_username
-                    )
-                )
+                'text': formatted_response
             }
         )
