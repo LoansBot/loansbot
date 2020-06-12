9c51a28c40906eaa0fde73850c604eaec4ba951a
Timothy Moore <mtimothy984@gmail.com>
2020-06-12 08:48:36 -0700

    Fix cwd issues

--- tests/helper.py
+++ tests/helper.py
@@ -1,4 +1,15 @@
+import os
 import sys
+
+curdir = os.getcwd()
+
+if os.path.split(curdir)[1] == 'tests':
+    print(f'cwd was={os.getcwd()}')
+    os.chdir(
+        os.path.split(os.path.split(os.getcwd())[0])[0]
+    )
+    print(f'cwd={os.getcwd()}')
+
 
 if 'src' not in sys.path:
     sys.path.append('src')
