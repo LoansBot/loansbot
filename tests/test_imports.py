f97f612ec63b0749edc408feac305b8a914de588
Timothy Moore <mtimothy984@gmail.com>
2020-06-15 06:41:49 -0700

    Fix import all trying folders instead of files

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -8,12 +8,13 @@ from importlib import import_module
 
 class Test(unittest.TestCase):
     def test_import_all(self):
-        for rootp, files, _ in os.walk('src'):
+        for rootp, _, files in os.walk('src'):
             for f in files:
                 if not f.endswith('.py'):
                     continue
                 fullpath = os.path.join(rootp, f)
                 modpath = fullpath[4:-3].replace(os.path.sep, '.')
+                print(f'trying {fullpath} via {modpath}')
                 import_module(modpath)
 
 
